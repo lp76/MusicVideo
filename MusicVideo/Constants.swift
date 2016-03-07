14c64db8071e4e98e2debed53ba842ccd86a1a7c
Luca Peduto <l.peduto@gmail.com>
2016-03-07 08:16:58 +0100

    Completed Part 4

---
+++ MusicVideo/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MusicVideo
+//
+//  Created by Luca Peduto on 07/03/16.
+//  Copyright © 2016 Eloquent Bit. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: AnyObject]
+
+typealias JSONArray = Array<AnyObject>
