93658b1fc6de52921d2505ca156f7273cc73234b
Luca Peduto <l.peduto@gmail.com>
2016-03-22 08:35:50 +0100

    Completed Part 14

---
+++ MusicVideo/MusicVideoTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MusicVideoTableViewCell.swift
+//  MusicVideo
+//
+//  Created by Luca Peduto on 20/03/16.
+//  Copyright © 2016 Eloquent Bit. All rights reserved.
+//
+
+import UIKit
+
+class MusicVideoTableViewCell: UITableViewCell {
+    
+    var video: Videos? {
+        didSet {
+            updateCell()
+        }
+    }
+    
+    @IBOutlet weak var musicImage: UIImageView!
+    
+    @IBOutlet weak var rank: UILabel!
+    
+    @IBOutlet weak var musicTitle: UILabel!
+    
+    func updateCell() {
+        musicTitle.text = video!.vName
+        rank.text = ("\(video!.vRank)")
+        musicImage.image = UIImage(named: "imageNotAvailable")
+    }
+}
