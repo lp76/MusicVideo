daf397f70811814eaa88072979e8f6a6dcd9d4b9
Luca Peduto <l.peduto@gmail.com>
2016-03-29 07:43:48 +0200

    Completed Part 17

--- MusicVideo/MusicVideoTVC.swift
+++ MusicVideo/MusicVideoTVC.swift
@@ -17,8 +17,14 @@ class MusicVideoTVC: UITableViewController {
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "reachabilityStatusChanged", name: "ReachStatusChanged", object: nil)
         
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "preferredFontChange", name: UIContentSizeCategoryDidChangeNotification, object: nil)
+        
         reachabilityStatusChanged()
         
+    }
+    
+    func preferredFontChange() {
+        print("The preferred Font has changed")
     }
     
     func didLoadData(videos: [Videos]) {
@@ -82,6 +88,7 @@ class MusicVideoTVC: UITableViewController {
     deinit
     {
         NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIContentSizeCategoryDidChangeNotification, object: nil)
     }
 
     // MARK: - Table view data source
--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -8,26 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController {
     
     var videos = [Videos]()
 
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var displayLabel: UILabel!
+//    @IBOutlet weak var tableView: UITableView!
+//    @IBOutlet weak var displayLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.dataSource = self
-        tableView.delegate = self
+        //tableView.dataSource = self
+        //tableView.delegate = self
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "reachabilityStatusChanged", name: "ReachStatusChanged", object: nil)
         
-        reachabilityStatusChanged()
+        //reachabilityStatusChanged()
         
         // Call API
-        let api = APIManager()
-        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=50/json", completion: didLoadData)
+        //let api = APIManager()
+        //api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=50/json", completion: didLoadData)
     }
     
     func didLoadData(videos: [Videos]) {
@@ -40,45 +40,45 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             print("name = \(item.vName)")
         }
         
-        tableView.reloadData()
+        //tableView.reloadData()
         
     }
     
-    func reachabilityStatusChanged() {
-        
-        switch reachabilityStatus {
-        case NOACCESS: view.backgroundColor = UIColor.redColor()
-            displayLabel.text = "No Internet"
-        case WIFI: view.backgroundColor = UIColor.greenColor()
-            displayLabel.text = "Reachable with WIFI"
-        case WWAN: view.backgroundColor = UIColor.yellowColor()
-            displayLabel.text = "Reachable with Cellular"
-        default: return
-        }
-    }
+//    func reachabilityStatusChanged() {
+//        
+//        switch reachabilityStatus {
+//        case NOACCESS: view.backgroundColor = UIColor.redColor()
+//            displayLabel.text = "No Internet"
+//        case WIFI: view.backgroundColor = UIColor.greenColor()
+//            displayLabel.text = "Reachable with WIFI"
+//        case WWAN: view.backgroundColor = UIColor.yellowColor()
+//            displayLabel.text = "Reachable with Cellular"
+//        default: return
+//        }
+//    }
     
     deinit
     {
         NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
     }
     
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return videos.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
-        let video = videos[indexPath.row]
-        cell.textLabel?.text = ("\(indexPath.row + 1)")
-        cell.detailTextLabel?.text = video.vName
-        
-        return cell
-    }
+//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        return 1
+//    }
+//    
+//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return videos.count
+//    }
+//    
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        
+//        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+//        let video = videos[indexPath.row]
+//        cell.textLabel?.text = ("\(indexPath.row + 1)")
+//        cell.detailTextLabel?.text = video.vName
+//        
+//        return cell
+//    }
 
 }
 
